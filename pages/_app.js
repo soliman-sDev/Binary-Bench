b32daa2c7036dd29fd7b703a972afaf892784011
soliman-sDev <202615672+soliman-sDev@users.noreply.github.com>
2025-05-25 15:09:42 +0300

    add: content

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }) {
   return (
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
   );
 }
